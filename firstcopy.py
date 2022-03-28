fa1e4dfd9c675f575066f354137671ca0b444a6e
deepti161988 <patel.deepti16@gmail.com>
2022-03-28 20:29:36 +0530

    changed

--- firstcopy.py
+++ firstcopy.py
@@ -1,2 +1,10 @@
-print("deepti")
-
+from lib2to3.pgen2 import driver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome(executable_path="C:\\BIkash Practice\\Python\\chromedriver98\\chromedriver.exe")
+driver.get("https://www.google.com/")
+print(driver.title)
+driver.maximize_window()
+driver.implicitly_wait(5)
+driver.minimize_window()
+#driver.quit()
